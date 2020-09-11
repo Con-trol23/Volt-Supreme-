ddb9d8f6fffc27aacef074105f3ed9c3d6354b14
Con-trol23 <71089503+Con-trol23@users.noreply.github.com>
2020-09-11 20:23:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 
 //this line enters the billing name on to the supremes website
-document.getElementById('order_billing_name').value = "Nelson Pompigne - Mognard"
+document.getElementById('order_billing_name').value = "Name"
 //this line enters the billing email on to the supremes website
-document.getElementById('order_email').value = "sybobotics@gmail.com"
+document.getElementById('order_email').value = "Email"
 //this line enters the billing phone number on to the supremes website
-document.getElementById('order_tel').value = "+351 932 073 140"
+document.getElementById('order_tel').value = "Phone Number"
 //this line clicks the terms and condos and the process payment on to the supremes website
 document.getElementsByClassName('iCheck-helper')[1].click();
 document.getElementsByClassName ("button checkout")[0].click();
 //this line enters the card credit_card_year
-document.getElementById('credit_card_year').value = "2025"
+document.getElementById('credit_card_year').value = "Credit card date"
 //this line enters the customers city
-document.getElementById('order_billing_city').value = "Cascais"
+document.getElementById('order_billing_city').value = "City"
 //this line enters the customers CVV
-document.getElementById('vval').value = "123"
+document.getElementById('vval').value = "CVV"
 //this line enters the customers Card number
-document.getElementById('cnb').value = "123"
+document.getElementById('cnb').value = "Card number"
 //need to Fix
-document.getElementById('credit_card_type').value = "Visa"
+document.getElementById('credit_card_type').value = "Credit Card type"
